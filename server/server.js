23fa90d31307a15b4cc3713025da2953d3c01f50
Roswaalem <132373323+Roswaalem@users.noreply.github.com>
2024-03-19 21:55:10 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,9 +22,10 @@ const fetchData = (summonerName, tagline) => {
                 data += chunk;
             });
             response.on('end', async () => {
-                const accountData = JSON.parse(data);
-                const puuid = accountData.puuid;
                 try {
+                    const accountData = JSON.parse(data);
+                    const puuid = accountData.puuid;
+
                     const getIdentifiers = await new Promise((resolve, reject) => {
                         https.get(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${api_key}`, (response) => {
                             let data = '';
@@ -46,36 +47,14 @@ const fetchData = (summonerName, tagline) => {
                                 data += chunk;
                             });
                             response.on('end', () => {
-                                const champions = JSON.parse(data);
-                                const championIds = Object.values(champions).map(champion => champion.championId);
-                                try {
-                                    const getChampionsData = new Promise((resolve, reject) => {
-                                        https.get(`https://ddragon.leagueoflegends.com/cdn/14.5.1/data/en_US/championFull.json`, (response) => {
-                                            let data = '';
-                                            response.on('data', (chunk) => {
-                                                data += chunk;
-                                            });
-                                            response.on('end', () => {
-                                                const championsData = JSON.parse(data);
-                                                const championNames = championIds.map(championId => championsData.keys[championId]);
-                                                resolve(championNames, championsData);
-                                            });
-                                        }).on('error', (error) => {
-                                            reject(error);
-                                        });
-                                    });
-
-                                    resolve(championsData, championNames);
-                                } catch (error) {
-                                    reject(error);
-                                }
+                                resolve(JSON.parse(data));
                             });
                         }).on('error', (error) => {
                             reject(error);
                         });
                     });
 
-                    resolve({ response1: accountData, response2: getIdentifiers, response3: getChampions});
+                    resolve({ response1: accountData, response2: getIdentifiers, response3: getChampions });
                 } catch (error) {
                     reject(error);
                 }
