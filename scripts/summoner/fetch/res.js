94bf5528ba40604c86633d502ed15533562f251a
idkVlad <valentin.daclin110802@gmail.com>
2024-03-20 12:03:05 +0100

    Updated server.js

--- scripts/summoner/fetch/res.js
+++ scripts/summoner/fetch/res.js
@@ -1,18 +1,24 @@
-import { api_key } from "../../data.js";
-
 document.addEventListener("DOMContentLoaded", function() {
     const params = new URLSearchParams(window.location.search);
     const summoner = params.get('summoner');
 
     const [summonerName, tagline] = summoner.split('#');
 
-    const url = `/proxy?summonerName=${summonerName}&tagline=${tagline}`;
+    const url = `https://bright-stardust-bbf42d.netlify.app/.netlify/functions/server?summonerName=${summonerName}&tagline=${tagline}`;
 
     fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
-            // Traitez les données reçues
-            console.log(data);
+            // Mettez à jour les éléments HTML avec les données récupérées
+            document.getElementById("nom_du_joueur").textContent = data.nomDuJoueur;
+            document.getElementById("niveau_du_joueur").textContent = data.niveauDuJoueur;
+            // Continuez à mettre à jour les autres éléments en fonction de vos données
         })
         .catch(error => console.error(error));
 });
+
