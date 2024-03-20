42b1eac7c538cb894be40cdadab8934ac8df73c4
idkVlad <valentin.daclin110802@gmail.com>
2024-03-20 11:48:15 +0100

    Updated server.js

--- proxy/server.js
+++ proxy/server.js
@@ -2,10 +2,9 @@ import { api_key } from "../scripts/data.js";
 
 exports.handler = async (event, context) => {
   const { summonerName, tagline } = event.queryStringParameters;
-  const API_KEY = 'YOUR_API_KEY'; // Remplacez par votre propre clé d'API
 
   try {
-    const response = await fetch(`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagline}?api_key=${API_KEY}`);
+    const response = await fetch(`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagline}?api_key=${api_key}`);
     const data = await response.json();
 
     return {
