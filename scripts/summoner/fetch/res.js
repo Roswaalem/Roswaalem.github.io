a5483c8869febffec70b1f83fbd3496cfe4614e0
idkVlad <valentin.daclin110802@gmail.com>
2024-03-20 12:14:55 +0100

    Update res.js endpoint

--- scripts/summoner/fetch/res.js
+++ scripts/summoner/fetch/res.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const [summonerName, tagline] = summoner.split('#');
 
-    const url = `/server?summonerName=${summonerName}&tagline=${tagline}`;
+    const url = `https://bright-stardust-bbf42d.netlify.app/.netlify/functions/server?summonerName=${summonerName}&tagline=${tagline}`;
 
     fetch(url)
         .then(response => {
