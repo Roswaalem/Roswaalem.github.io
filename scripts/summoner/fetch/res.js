93585305c95cd26e509e273762138b4ee39616a7
idkVlad <valentin.daclin110802@gmail.com>
2024-03-20 14:27:25 +0100

    Update url to fetch

--- scripts/summoner/fetch/res.js
+++ scripts/summoner/fetch/res.js
@@ -17,10 +17,10 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector("h1").innerText = data.response1.gameName;
             document.getElementById("summoner_name").innerText = data.response1.gameName + "#" + data.response1.tagLine;
             document.querySelector("h5").innerText = data.response1.puuid;
-/*
+
             document.getElementById("summoner_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/profileicon/" + data.response2.profileIconId + ".png";
             document.getElementById("summoner_level").innerText = data.response2.summonerLevel;
-
+/*
             document.getElementById("champion1_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/" + data.response3[0] + ".png";
             document.getElementById("champion1_name").innerText = data.response3[0];
             document.getElementById("champion1_mastery").innerText = data.response3[0].masteryLevel;
