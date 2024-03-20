4e734e5a0e8b18a23ce7a36862bba3e09553e1ad
idkVlad <valentin.daclin110802@gmail.com>
2024-03-20 14:35:56 +0100

    Update url to fetch

--- scripts/summoner/fetch/res.js
+++ scripts/summoner/fetch/res.js
@@ -21,14 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("summoner_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/profileicon/" + data.response2.profileIconId + ".png";
             document.getElementById("summoner_level").innerText = data.response2.summonerLevel;
 
-            document.getElementById("champion1_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/" + data.response3.championId + ".png";
-            document.getElementById("champion1_name").innerText = data.response3.championId;
-            document.getElementById("champion1_mastery").innerText = data.response3.championPoints;
+            document.getElementById("champion1_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/" + data.response3[0].championId + ".png";
+            document.getElementById("champion1_name").innerText = data.response3[0].championId;
+            document.getElementById("champion1_mastery").innerText = data.response3[0].championPoints;
 
-            document.getElementById("champion2_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/" + data.response3.championId + ".png";
+            document.getElementById("champion2_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/" + data.response3[1].championId + ".png";
             document.getElementById("champion2_name").innerText = data.response3[1];
 
-            document.getElementById("champion3_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/" + data.response3.championId + ".png";
+            document.getElementById("champion3_icon").src = "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/" + data.response3[2].championId + ".png";
             document.getElementById("champion3_name").innerText = data.response3[2];
 
             console.log(data);
